75bfc8ea81c9b3752715e96121d43f3f25708a7e
-k <slowdive@me.com>
2019-09-25 20:01:05 -0700

    Revert "Fix insight link"
    This reverts commit dd08268f03a5ef18636c6a39befca1c02b02b91b.

--- js/index.js
+++ js/index.js
@@ -4,10 +4,10 @@ var socket;
 var paymentCycle;
 
 var mainnetProvider = 'https://insight.axecore.net';
-var mainnetPrefix = '/api';
+var mainnetPrefix = '/insight-api';
 
 var testnetProvider = 'https://testnet-insight.axecore.net';
-var testnetPrefix = '/api';
+var testnetPrefix = '/insight-api';
 
 var init = function(network, provider, prefix) {
     var gov = new Bitcore.GovObject.Proposal();
